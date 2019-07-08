0e37f8fb47f3377ba4d8f07eacde3dea080e9774
ravd45 <r.velazqez@hotmail.com>
2019-07-08 13:14:41 -0500

    arreglo consulta indicadores

--- application/models/Estadistica_e_indicadores_xcct_model__.php
+++ application/models/Estadistica_e_indicadores_xcct_model__.php
@@ -13,9 +13,9 @@ class Estadistica_e_indicadores_xcct_model extends CI_Model
     SUM(est.alumn_m_t) as alumn_m_t, SUM(est.alumn_h_t) as alumn_h_t, SUM(est.alumn_t_t) as alumn_t_t,
     SUM(est.alumn_t_1) as alumn_t_1, SUM(est.alumn_t_2) as alumn_t_2, SUM(est.alumn_t_3) as alumn_t_3,
     SUM(est.alumn_t_4) as alumn_t_4, SUM(est.alumn_t_5) as alumn_t_5, SUM(est.alumn_t_6) as alumn_t_6');
-    $this->db->from('estadistica_e_indicadores_xcct as est');
-    $this->db->join('escuela as es', 'es.id_cct = est.id_cct', 'left');
-    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio', 'left');
+    $this->db->from('escuela as es');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
+    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('ciclo as ci', 'est.id_ciclo = ci.id_ciclo');
     if($id_municipio>0){
@@ -33,9 +33,9 @@ class Estadistica_e_indicadores_xcct_model extends CI_Model
     SUM(est.alumn_m_t) as alumn_m_t, SUM(est.alumn_h_t) as alumn_h_t, SUM(est.alumn_t_t) as alumn_t_t,
     SUM(est.alumn_t_1) as alumn_t_1, SUM(est.alumn_t_2) as alumn_t_2, SUM(est.alumn_t_3) as alumn_t_3,
     SUM(est.alumn_t_4) as alumn_t_4, SUM(est.alumn_t_5) as alumn_t_5, SUM(est.alumn_t_6) as alumn_t_6');
-    $this->db->from('estadistica_e_indicadores_xcct as est');
-    $this->db->join('escuela as es', 'es.id_cct = est.id_cct', 'left');
-    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio', 'left');
+    $this->db->from('escuela as es');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
+    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('modalidad as mo', 'es.id_modalidad = mo.id_modalidad');
     $this->db->join('subsostenimiento as sso', 'es.id_subsostenimiento = sso.id_subsostenimiento');
@@ -57,9 +57,9 @@ class Estadistica_e_indicadores_xcct_model extends CI_Model
     SUM(est.alumn_m_t) as alumn_m_t, SUM(est.alumn_h_t) as alumn_h_t, SUM(est.alumn_t_t) as alumn_t_t,
     SUM(est.alumn_t_1) as alumn_t_1, SUM(est.alumn_t_2) as alumn_t_2, SUM(est.alumn_t_3) as alumn_t_3,
     SUM(est.alumn_t_4) as alumn_t_4, SUM(est.alumn_t_5) as alumn_t_5, SUM(est.alumn_t_6) as alumn_t_6');
-    $this->db->from('estadistica_e_indicadores_xcct as est');
-    $this->db->join('escuela as es', 'es.id_cct = est.id_cct', 'left');
-    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio', 'left');
+    $this->db->from('escuela as es');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
+    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('modalidad as mo', 'es.id_modalidad = mo.id_modalidad');
     $this->db->join('subsostenimiento as sso', 'es.id_subsostenimiento = sso.id_subsostenimiento');
@@ -98,8 +98,8 @@ class Estadistica_e_indicadores_xcct_model extends CI_Model
     SUM(est.alumn_m_t) as alumn_m_t, SUM(est.alumn_h_t) as alumn_h_t, SUM(est.alumn_t_t) as alumn_t_t,
     SUM(est.alumn_t_1) as alumn_t_1, SUM(est.alumn_t_2) as alumn_t_2, SUM(est.alumn_t_3) as alumn_t_3,
     SUM(est.alumn_t_4) as alumn_t_4, SUM(est.alumn_t_5) as alumn_t_5, SUM(est.alumn_t_6) as alumn_t_6');
-    $this->db->from('estadistica_e_indicadores_xcct as est');
-    $this->db->join('escuela as es', 'es.id_cct = est.id_cct', 'left');
+    $this->db->from('escuela as es');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('subsostenimiento as sso', '`es`.`id_subsostenimiento` = `sso`.`id_subsostenimiento`');
     $this->db->join('supervision as su', '`es`.`id_supervision` = `su`.`id_supervision`');
@@ -125,9 +125,9 @@ class Estadistica_e_indicadores_xcct_model extends CI_Model
     SUM(est.alumn_m_t) as alumn_m_t, SUM(est.alumn_h_t) as alumn_h_t, SUM(est.alumn_t_t) as alumn_t_t,
     SUM(est.alumn_t_1) as alumn_t_1, SUM(est.alumn_t_2) as alumn_t_2, SUM(est.alumn_t_3) as alumn_t_3,
     SUM(est.alumn_t_4) as alumn_t_4, SUM(est.alumn_t_5) as alumn_t_5, SUM(est.alumn_t_6) as alumn_t_6');
-    $this->db->from('estadistica_e_indicadores_xcct as est');
-    $this->db->join('escuela as es', 'es.id_cct = est.id_cct', 'left');
-    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio', 'left');
+    $this->db->from('escuela as es');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
+    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('modalidad as mo', 'es.id_modalidad = mo.id_modalidad');
     $this->db->join('subsostenimiento as sso', 'es.id_subsostenimiento = sso.id_subsostenimiento');
@@ -156,9 +156,9 @@ class Estadistica_e_indicadores_xcct_model extends CI_Model
     SUM(est.alumn_m_t) as alumn_m_t, SUM(est.alumn_h_t) as alumn_h_t, SUM(est.alumn_t_t) as alumn_t_t,
     SUM(est.alumn_t_1) as alumn_t_1, SUM(est.alumn_t_2) as alumn_t_2, SUM(est.alumn_t_3) as alumn_t_3,
     SUM(est.alumn_t_4) as alumn_t_4, SUM(est.alumn_t_5) as alumn_t_5, SUM(est.alumn_t_6) as alumn_t_6');
-    $this->db->from('estadistica_e_indicadores_xcct as est');
-    $this->db->join('escuela as es', 'es.id_cct = est.id_cct', 'left');
-    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio', 'left');
+    $this->db->from('escuela as es');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
+    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('modalidad as mo', 'es.id_modalidad = mo.id_modalidad');
     $this->db->join('subsostenimiento as sso', 'es.id_subsostenimiento = sso.id_subsostenimiento');
@@ -218,10 +218,10 @@ class Estadistica_e_indicadores_xcct_model extends CI_Model
 +SUM(est.docente_h_idiomas)
 +SUM(est.docente_m_idiomas)) ,SUM(est.docentes_t_g)) AS docentes_t_g,
     SUM(est.directivo_m_congrup) as directivo_m_congrup, SUM(est.directivo_h_congrup) as directivo_h_congrup, SUM(est.directivo_m_congrup)+SUM(est.directivo_h_congrup) as directivo_t_congrup,
+    SUM(est.directivo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup) as directivo_h_singrup, SUM(est.directivo_m_singrup)+SUM(est.directivo_h_singrup) as directivo_t_singrup');
     $this->db->from('escuela as es');
-    SUM(estfromectivo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup) as directivo_h_singrup, SUM(est.directivo_m_singrup)+S) as joinctivo_t_singrup');
-    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct'UM(est.directivo_h_singrup, 'left');
-    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio', 'left');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
+    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('ciclo as ci', 'est.id_ciclo = ci.id_ciclo');
     if($id_municipio>0){
@@ -256,10 +256,10 @@ class Estadistica_e_indicadores_xcct_model extends CI_Model
 +SUM(est.docente_h_idiomas)
 +SUM(est.docente_m_idiomas)) ,SUM(est.docentes_t_g)) AS docentes_t_g,
 SUM(est.directivo_m_congrup) as directivo_m_congrup, SUM(est.directivo_h_congrup) as directivo_h_congrup, SUM(est.directivo_m_congrup)+SUM(est.directivo_h_congrup) as directivo_t_congrup,
+SUM(est.directivo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup) as directivo_h_singrup, SUM(est.directivo_m_singrup)+SUM(est.directivo_h_singrup) as directivo_t_singrup');
     $this->db->from('escuela as es');
-SUM(est.dirfromvo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup) as directivo_h_singrup, SUM(est.directivo_m_singrup)+SUM(s directijoin_singrup');
-    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct'est.directivo_h_singrup) a, 'left');
-    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio', 'left');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
+    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('modalidad as mo', 'es.id_modalidad = mo.id_modalidad');
     $this->db->join('subsostenimiento as sso', 'es.id_subsostenimiento = sso.id_subsostenimiento');
@@ -298,10 +298,10 @@ SUM(est.dirfromvo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup
 +SUM(est.docente_h_idiomas)
 +SUM(est.docente_m_idiomas)) ,SUM(est.docentes_t_g)) AS docentes_t_g,
 SUM(est.directivo_m_congrup) as directivo_m_congrup, SUM(est.directivo_h_congrup) as directivo_h_congrup, SUM(est.directivo_m_congrup)+SUM(est.directivo_h_congrup) as directivo_t_congrup,
+SUM(est.directivo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup) as directivo_h_singrup, SUM(est.directivo_m_singrup)+SUM(est.directivo_h_singrup) as directivo_t_singrup');
     $this->db->from('escuela as es');
-SUM(est.dirfromvo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup) as directivo_h_singrup, SUM(est.directivo_m_singrup)+SUM(s directijoin_singrup');
-    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct'est.directivo_h_singrup) a, 'left');
-    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio', 'left');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
+    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('modalidad as mo', 'es.id_modalidad = mo.id_modalidad');
     $this->db->join('subsostenimiento as sso', 'es.id_subsostenimiento = sso.id_subsostenimiento');
@@ -358,9 +358,9 @@ SUM(est.dirfromvo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup
 +SUM(est.docente_h_idiomas)
 +SUM(est.docente_m_idiomas)) ,SUM(est.docentes_t_g)) AS docentes_t_g,
     SUM(est.directivo_m_congrup) as directivo_m_congrup, SUM(est.directivo_h_congrup) as directivo_h_congrup, SUM(est.directivo_m_congrup)+SUM(est.directivo_h_congrup) as directivo_t_congrup,
+    SUM(est.directivo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup) as directivo_h_singrup, SUM(est.directivo_m_singrup)+SUM(est.directivo_h_singrup) as directivo_t_singrup');
     $this->db->from('escuela as es');
-    SUM(estfromectivo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup) as directivo_h_singrup, SUM(est.directivo_m_singrup)+S) as joinctivo_t_singrup');
-    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct'UM(est.directivo_h_singrup, 'left');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('subsostenimiento as sso', '`es`.`id_subsostenimiento` = `sso`.`id_subsostenimiento`');
     $this->db->join('supervision as su', '`es`.`id_supervision` = `su`.`id_supervision`');
@@ -403,10 +403,10 @@ SUM(est.dirfromvo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup
 +SUM(est.docente_h_idiomas)
 +SUM(est.docente_m_idiomas)) ,SUM(est.docentes_t_g)) AS docentes_t_g,
 SUM(est.directivo_m_congrup) as directivo_m_congrup, SUM(est.directivo_h_congrup) as directivo_h_congrup, SUM(est.directivo_m_congrup)+SUM(est.directivo_h_congrup) as directivo_t_congrup,
+SUM(est.directivo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup) as directivo_h_singrup, SUM(est.directivo_m_singrup)+SUM(est.directivo_h_singrup) as directivo_t_singrup');
     $this->db->from('escuela as es');
-SUM(est.dirfromvo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup) as directivo_h_singrup, SUM(est.directivo_m_singrup)+SUM(s directijoin_singrup');
-    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct'est.directivo_h_singrup) a, 'left');
-    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio', 'left');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
+    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('modalidad as mo', 'es.id_modalidad = mo.id_modalidad');
     $this->db->join('subsostenimiento as sso', 'es.id_subsostenimiento = sso.id_subsostenimiento');
@@ -452,10 +452,10 @@ SUM(est.dirfromvo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup
 +SUM(est.docente_h_idiomas)
 +SUM(est.docente_m_idiomas)) ,SUM(est.docentes_t_g)) AS docentes_t_g,
 SUM(est.directivo_m_congrup) as directivo_m_congrup, SUM(est.directivo_h_congrup) as directivo_h_congrup, SUM(est.directivo_m_congrup)+SUM(est.directivo_h_congrup) as directivo_t_congrup,
+SUM(est.directivo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup) as directivo_h_singrup, SUM(est.directivo_m_singrup)+SUM(est.directivo_h_singrup) as directivo_t_singrup');
     $this->db->from('escuela as es');
-SUM(est.dirfromvo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup) as directivo_h_singrup, SUM(est.directivo_m_singrup)+SUM(s directijoin_singrup');
-    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct'est.directivo_h_singrup) a, 'left');
-    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio', 'left');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
+    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('modalidad as mo', 'es.id_modalidad = mo.id_modalidad');
     $this->db->join('subsostenimiento as sso', 'es.id_subsostenimiento = sso.id_subsostenimiento');
@@ -504,9 +504,9 @@ SUM(est.dirfromvo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup
     	SUM(est.grupos_6) AS grupos_6,
     	SUM(est.grupos_multi) AS grupos_multi,
     	SUM(est.grupos_t) AS grupos_t');
-    $this->db->from('estadistica_e_indicadores_xcct as est');
-    $this->db->join('escuela as es', 'es.id_cct = est.id_cct', 'left');
-    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio', 'left');
+    $this->db->from('escuela as es');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
+    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('ciclo as ci', 'est.id_ciclo = ci.id_ciclo','left');
     if($id_municipio>0){
@@ -530,9 +530,9 @@ SUM(est.dirfromvo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup
 	SUM(est.grupos_6) AS grupos_6,
 	SUM(est.grupos_multi) AS grupos_multi,
 	SUM(est.grupos_t) AS grupos_t');
-    $this->db->jfrom'estadistica_e_indicadores_xcct as est');
-    $this->db->fjoin'escuela as es', 'es.id_cct = est.id_cct', 'left');
-    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio', 'left');
+    $this->db->from('escuela as es');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
+    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('modalidad as mo', 'es.id_modalidad = mo.id_modalidad');
     $this->db->join('subsostenimiento as sso', 'es.id_subsostenimiento = sso.id_subsostenimiento');
@@ -560,9 +560,9 @@ SUM(est.dirfromvo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup
 	SUM(est.grupos_6) AS grupos_6,
 	SUM(est.grupos_multi) AS grupos_multi,
 	SUM(est.grupos_t) AS grupos_t');
-    $this->db->jfrom'estadistica_e_indicadores_xcct as est');
-    $this->db->fjoin'escuela as es', 'es.id_cct = est.id_cct', 'left');
-    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio', 'left');
+    $this->db->from('escuela as es');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
+    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('modalidad as mo', 'es.id_modalidad = mo.id_modalidad');
     $this->db->join('subsostenimiento as sso', 'es.id_subsostenimiento = sso.id_subsostenimiento');
@@ -604,8 +604,8 @@ SUM(est.dirfromvo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup
       SUM(est.grupos_6) AS grupos_6,
       SUM(est.grupos_multi) AS grupos_multi,
       SUM(est.grupos_t) AS grupos_t');
-    $this->db->from('estadistica_e_indicadores_xcct as est');
-    $this->db->join('escuela as es', 'es.id_cct = est.id_cct', 'left');
+    $this->db->from('escuela as es');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('subsostenimiento as sso', 'es.id_subsostenimiento = sso.id_subsostenimiento');
     $this->db->join('supervision as su', '`es`.`id_supervision` = `su`.`id_supervision`');
@@ -637,9 +637,9 @@ SUM(est.dirfromvo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup
   SUM(est.grupos_6) AS grupos_6,
   SUM(est.grupos_multi) AS grupos_multi,
   SUM(est.grupos_t) AS grupos_t');
-    $this->db->from('estadistica_e_indicadores_xcct as est');
-    $this->db->join('escuela as es', 'es.id_cct = est.id_cct', 'left');
-    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio', 'left');
+    $this->db->from('escuela as es');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
+    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('modalidad as mo', 'es.id_modalidad = mo.id_modalidad');
     $this->db->join('subsostenimiento as sso', 'es.id_subsostenimiento = sso.id_subsostenimiento');
@@ -674,9 +674,9 @@ SUM(est.dirfromvo_m_singrup) as directivo_m_singrup, SUM(est.directivo_h_singrup
   SUM(est.grupos_6) AS grupos_6,
   SUM(est.grupos_multi) AS grupos_multi,
   SUM(est.grupos_t) AS grupos_t');
-    $this->db->from('estadistica_e_indicadores_xcct as est');
-    $this->db->join('escuela as es', 'es.id_cct = est.id_cct', 'left');
-    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio', 'left');
+    $this->db->from('escuela as es');
+    $this->db->join('estadistica_e_indicadores_xcct as est', 'es.id_cct = est.id_cct');
+    $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio');
     $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
     $this->db->join('modalidad as mo', 'es.id_modalidad = mo.id_modalidad');
     $this->db->join('subsostenimiento as sso', 'es.id_subsostenimiento = sso.id_subsostenimiento');
