89c7df12ab0f3dde9a0818c2549f9e82554b9561
EloisaHA <helo9211@gmail.com>
2019-09-23 12:46:58 -0500

    validaciones en filtro por cct y turno

--- assets/js/estadistica_pemc/busquedaxcct.js
+++ assets/js/estadistica_pemc/busquedaxcct.js
@@ -2,7 +2,10 @@ $("#busqueda_cct_pemc").click(function(e){
   e.preventDefault();
   let cct=$("#cct_pemc").val();
   let turno=$("#turno_pemc").val();
-  if(cct=="" && turno=="Seleccione Turno"){
+  // console.log(cct);
+  // console.log(turno);
+  console.log(validadatos());
+  if(validadatos()==false){
     alert("Debe seleccionar un turno y agregar una cct");
   }else{
 
@@ -42,6 +45,19 @@ function validacct(){
   }
   else if ($("#cct_pemc").val().length>8) {
     bandera=false;
+  }
+
+  return bandera;
+}
+
+function validadatos(){
+  bandera=false;
+  let cct=$("#cct_pemc").val();
+  let turno=$("#turno_pemc").val();
+  if(cct!=""){
+    if(turno!="Seleccione un turno"){
+      bandera=true;
+    }
   }
 
   return bandera;
