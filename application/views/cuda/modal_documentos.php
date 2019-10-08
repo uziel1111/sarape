67a09af61ab2eabccc8380e9a1fc48e80f7cda87
ravd45 <r.velazqez@hotmail.com>
2019-10-08 15:43:49 -0500

    ajuste modal

--- application/views/cuda/modal_documentos.php
+++ application/views/cuda/modal_documentos.php
@@ -8,13 +8,14 @@
 	
 	<div class="row">
 		<div class="col-sm text-right">
+			<!-- <a>El archivo está cargando</a> -->
 			<a class="btn btn-success mb-2" target="blank_" href="http://qual-edu.org/levantamiento_de_requerimientos/<?= $value['url_comple']?>" role="button"><i class="fas fa-file-download"></i> Descargar</a>   
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-sm">
 			<?php if ($tipo == 'pdf') { ?>
-				<iframe src="https://docs.google.com/viewer?url=http://qual-edu.org/levantamiento_de_requerimientos/<?= $value['url_comple']?>&embedded=true" width="100%" height="500" style="border: none;"></iframe>
+				<iframe src="http://qual-edu.org/levantamiento_de_requerimientos/<?= $value['url_comple']?>" width="100%" height="500" style="border: none;"></iframe>
 			<?php } else { ?>
 				<img src="http://qual-edu.org/levantamiento_de_requerimientos/<?= $value['url_comple']?>" width="100%" height="100%" style="border: none;" />
 			<?php } ?>
