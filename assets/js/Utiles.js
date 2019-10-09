ca4531df64876fbbe68fefdf631cbd79a0dc4ed5
EloisaHA <helo9211@gmail.com>
2019-10-09 09:05:32 -0500

    modificacion en la funcion showPDF en el archivo Utiles.

--- assets/js/Utiles.js
+++ assets/js/Utiles.js
@@ -6,7 +6,7 @@ var Utiles = {
   path_docs : "http://www.sarape.gob.mx/assets/docs/",
 
   showPDF : function(idModal, urlPdf){
-    var dom = '<iframe src="https://docs.google.com/viewerng/viewer?url='+this.path_docs+urlPdf+'&embedded=true" width="100%" height="500" style="border: none;"></iframe>';
+    var dom = '<iframe src="'+this.path_docs+urlPdf+'" width="100%" height="500" style="border: none;"></iframe>';
     $("#"+idModal +" .modal-body").empty();
     $("#"+idModal +" .modal-body").html(dom);
     $("#"+idModal).modal("show");
