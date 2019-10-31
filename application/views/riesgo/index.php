827137cd32937a28b3424d1528d1da204447ff16
uziel1111 <uziel.123@gmail.com>
2019-10-31 09:53:37 -0600

    ajuste pie riesgo

--- application/views/riesgo/index.php
+++ application/views/riesgo/index.php
@@ -21,9 +21,9 @@
          <div class="card mb-3 card-style-1 mt-3">
             <div class="card-header card-1-header bgcolor-2 text-white">Resultados de búsqueda</div>
                 <div class="card-body">
-      <div id="dv_graf_riesgo_mun_zona"></div>
+      <div id="dv_graf_riesgo_mun_zona" class="table-responsive"></div>
       <div class="table-responsive" id="dv_tabla_riesgo_mun_zona"></div>
-      <div id="dv_tab_riesgo_mun_zona"></div>
+      <div id="dv_tab_riesgo_mun_zona" class="table-responsive"></div>
       <div class="table-responsive" id="dv_tablam_riesgo_mun_zona"></div>
      <!--  <div class='alert alert-success' role='alert'><h4 class="h4">Alumnos que posiblemente han abandonado la escuela: <span class="h3 text-white badge badge-secondary" id="total_bajas_muni">...</span></h4></div> -->
             </div><!-- card-body -->
