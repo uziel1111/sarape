2aa0b81b6d01758ba92c571071f78371a24513fe
ravd45 <r.velazqez@hotmail.com>
2019-11-06 11:05:29 -0600

    videotutoriales

--- application/views/ruta/login.php
+++ application/views/ruta/login.php
@@ -9,48 +9,143 @@
   <link href="<?= base_url('assets/css/login.css'); ?>" rel="stylesheet" media="screen">
 </head>
 <body>
+  <style>
+      .vt {
+    /* cambia estos dos valores para definir el tamaño de tu círculo */
+    height: 200px;
+    width: 200px !important;
+    /* los siguientes valores son independientes del tamaño del círculo */
+    background-repeat: no-repeat;
+    background-position: 50%;
+    border-radius: 50%;
+    background-size: 100% auto;
+  }
+  .li {
+    position: relative;
+    display: inline-block;
+    width: 23%;
+    margin: 1%;
+    text-align: left;
+    font-size: 12px;
+  }
+
+  li .link i {
+    z-index: 4;
+    opacity: 0;
+    position: absolute;
+    left: 210%;
+    bottom: 0%;
+    margin: 0px 0px -24px -24px;
+    width: 48px !important;
+    height: 48px;
+    background: url(https://i.imgur.com/FZaEYsL.png) no-repeat center center;
+    border-radius: 50%;
+    box-shadow: 0px 0px 40px rgba(0, 0, 0, 1);
+    transition: all 0.20s ease-out;
+  }
+
+  li .link img {
+    
+    transition: all 0.20s ease-out;
+    margin-top: -2%;
+    margin-bottom: -15%;
+  }
+
+  li:hover .link i {
+    opacity: .8;
+    bottom: 50%;
+  }
+
+  li:hover .link img {
+    transform: rotate(0deg) xscale(1.03);
+  }
+
+  </style>
   <div class="container-login" style="background-image: url('<?= base_url('assets/img/bg-01.jpg');?>');">
-    <div class="wrap-login">
-      <div class="login-form validate-form">
-        <div class="card">
-          <div class="card-body">
-            <center>
-              <img class="img-fluid" src="<?= base_url('assets/img/logo.png'); ?>" alt="">
-              <h5 class="card-title mt-3">Programa Escolar de Mejora Continua (PEMC)</h5>
-              <h4 class="card-title mt-3">Iniciar Sesión</h4>
-              <h6>Utilice el usuario y contraseña proporcionados para utilizar SIECEC</h6>
-            </center>
-            <center class="mensaje-terminado"><?=$this->session->flashdata(MESSAGEREQUEST);?></center>
-            <?= form_open('Rutademejora/acceso');?>
-            <div class="form-group">
-              <label for="usuario">CCT</label>               <input type="text" name="usuario" value="" id="usuario" class="form-control" required="required" autofocus="autofocus">
-            </div>
-            <div class="form-group">
-              <label for="password">Turno</label>
-              <select class="form-control" name="turno_id">
-                <option value="0">Seleccionar turno</option>
-                <option value="1">Matutino</option>
-                <option value="2">Vespertino</option>
-                <option value="3">Nocturno</option>
-                <option value="4">Discontinuo</option>
-                <option value="5">Mixto</option>
-              </select>
-            </div>
-            <div class="form-group">
-              <label for="password">Contraseña</label><input type="password" name="password" value="" id="password" class="form-control">
-
-            </div>
-            <div class="text-center">
-              <button type="submit" class="btn btn-primary btn-login center">Iniciar Sesión</button>
-            </div></form>
-          </div>
-        </div>
-
-      </div>
+    <div class="row">
+     <div class="col-md-12">
+        <div class="wrap-login">
+         <div class="login-form validate-form">
+           <div class="card">
+             <div class="card-body">
+               <center>
+                 <img class="img-fluid" src="<?= base_url('assets/img/logo.png'); ?>" alt="">
+                 <h5 class="card-title mt-3">Programa Escolar de Mejora Continua (PEMC)</h5>
+                 <h4 class="card-title mt-3">Iniciar Sesión</h4>
+                 <h6>Utilice el usuario y contraseña proporcionados para utilizar SIECEC</h6>
+               </center>
+               <center class="mensaje-terminado"><?=$this->session->flashdata(MESSAGEREQUEST);?></center>
+               <?= form_open('Rutademejora/acceso');?>
+               <div class="form-group">
+                 <label for="usuario">CCT</label>               <input type="text" name="usuario" value="" id="usuario" class="form-control" required="required" autofocus="autofocus">
+               </div>
+               <div class="form-group">
+                 <label for="password">Turno</label>
+                 <select class="form-control" name="turno_id">
+                   <option value="0">Seleccionar turno</option>
+                   <option value="1">Matutino</option>
+                   <option value="2">Vespertino</option>
+                   <option value="3">Nocturno</option>
+                   <option value="4">Discontinuo</option>
+                   <option value="5">Mixto</option>
+                 </select>
+               </div>
+               <div class="form-group">
+                 <label for="password">Contraseña</label><input type="password" name="password" value="" id="password" class="form-control">
+       
+               </div>
+               <div class="text-center">
+                 <button type="submit" class="btn btn-primary btn-login center">Iniciar Sesión</button>
+               </div></form>
+             </div>
+           </div>
+       
+         </div>
+       </div>
+     </div>
+    </div>
+    
+    <div class="row offset-md-1">
+    
+        
+        <li class="li">
+           <a class="link" data-toggle="modal" data-target="#T1_SARAPE">
+             <i></i>
+             <img src="<?= base_url('assets/vid/T1.png')?>" class="vt" alt=""></a>
+           </a>
+         </li>
+       
+     
     </div>
   </div><!-- /container -->
+  <div class="modal fade bd-example-modal-lg" id="T1_SARAPE" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog modal-lg" role="document" style="height: 1200px !important;">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title" id="exampleModalLabel">Tutorial 1</h4>
+          <button type="button" class="close" id='cerrar' data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-12" id="VTL1">
+              <iframe width="100%" height="415" src="https://www.youtube.com/embed/S5HT3mqxs3w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <script src="<?= base_url('assets/jquery-3.3.1.min.js'); ?>"></script>
   <script src="<?= base_url('assets/bootstrap-411/js/bootstrap.min.js'); ?>"></script>
+  <script>
+    $('#cerrar').click(function(event) {
+      $('#VTL1').empty();
+      $('#VTL1').html('<iframe width="100%" height="415" src="https://www.youtube.com/embed/S5HT3mqxs3w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+    });
+  </script>
 
 </body>
 </html>
--- application/views/ruta/rutademejora/ayuda.php
+++ application/views/ruta/rutademejora/ayuda.php
@@ -1,30 +1,191 @@
+<style>
+	.vt {
+		/* cambia estos dos valores para definir el tamaño de tu círculo */
+		height: 250px;
+		width: 250px;
+		/* los siguientes valores son independientes del tamaño del círculo */
+		background-repeat: no-repeat;
+		background-position: 50%;
+		border-radius: 50%;
+		background-size: 100% auto;
+	}
+	.li {
+		position: relative;
+		display: inline-block;
+		width: 23%;
+		margin: 1%;
+		text-align: left;
+		font-size: 12px;
+	}
+
+	li .link i {
+		z-index: 4;
+		opacity: 0;
+		position: absolute;
+		left: 50%;
+		bottom: 0%;
+		margin: 0px 0px -24px -24px;
+		width: 48px;
+		height: 48px;
+		background: url(https://i.imgur.com/FZaEYsL.png) no-repeat center center;
+		border-radius: 50%;
+		box-shadow: 0px 0px 40px rgba(0, 0, 0, 1);
+		transition: all 0.20s ease-out;
+	}
+
+	li .link img {
+		width: 100%;
+		transition: all 0.20s ease-out;
+		margin-top: -2%;
+		margin-bottom: -15%;
+	}
+
+	li:hover .link i {
+		opacity: .8;
+		bottom: 50%;
+	}
+
+	li:hover .link img {
+		transform: rotate(0deg) xscale(1.03);
+	}
+</style>
 <div class="tab-pane fade active show" id="nav-ayuda" role="tabpanel" aria-labelledby="nav-ayuda-tab">
 	<div class="card bg-light mb-3 center">
-	<!-- 	<div class="card-header"><span class="badge badge-secondary h5 text-white">1.</span> En este ciclo escolar quiero que mi escuela (Misión)</div>
-		<div class="card-body">
-			<p class="card-text">En esta sección se hace una descripción breve (de no más de 80 palabras aproximadamente) que clarifique cuál es la contribución que debe hacer la escuela a la comunidad donde radica, dónde se verá su impacto positivo y de qué forma deberá ser vista por quienes interactúan con ella (alumnos, padres de familia, autoridades locales, sociedad en general)</p>
+		<p>
+			<img src="<?= base_url('assets/img/mapa/mision.png')?>" width="200" height="200" alt="">
+			<img src="<?= base_url('assets/img/mapa/objetivos.png')?>" width="200" height="200" alt="">
+			<img src="<?= base_url('assets/img/mapa/actividades.png')?>" width="200" height="200" alt="">
+			<img src="<?= base_url('assets/img/mapa/avances.png')?>" width="200" height="200" alt="">
+			<img src="<?= base_url('assets/img/mapa/fin.png')?>" width="100" height="200" alt="">
+			<br>
+			<u><a style="padding-left: 7em; cursor: hand; cursor: pointer; color: blue;" id="img_mision">Ir a misión</a></u>
+			<a style="padding-left: 7em; cursor: hand; cursor: pointer;" >Ir a objetivos</a>
+			<a style="padding-left: 7em; cursor: hand; cursor: pointer;" >Ir a acciones</a>
+			<u><a style="padding-left: 7em; cursor: hand; cursor: pointer; color: blue;" id="img_avances">Ir a avances</a></p></u>
+			<br>
+			
+			
+
+			<div class="row">	
+				<li class="li">
+					<a class="link" data-toggle="modal" data-target="#T1_SARAPE">
+						<i></i>
+						<img src="<?= base_url('assets/vid/T1.png')?>" class="vt" alt=""></a>
+					</a>
+				</li>
+				<li class="li">
+					<a class="link" data-toggle="modal" data-target="#T2_SARAPE">
+						<i></i>
+						<img src="<?= base_url('assets/vid/T2.png')?>" class="vt" alt=""></a>
+					</a>
+				</li>
+				<li class="li">
+					<a class="link" data-toggle="modal" data-target="#T3_SARAPE">
+						<i></i>
+						<img src="<?= base_url('assets/vid/T3.png')?>" class="vt" alt=""></a>
+					</a>
+				</li>
+				<li class="li">
+					<a class="link" data-toggle="modal" data-target="#T4_SARAPE">
+						<i></i>
+						<img src="<?= base_url('assets/vid/T4.png')?>" class="vt" alt=""></a>
+					</a>
+				</li>
+				<br>	<br>	
+			</div>
 		</div>
 	</div>
-	<div class="card bg-light mb-3">
-		<div class="card-header"><span class="badge badge-secondary h5 text-white">3.</span> Objetivos y sus metas</div>
-		<div class="card-body">
-			<p class="card-text">Para la prioridad seleccionada escribe un objetivo que inicie con uno de los siguientes verbos (aumentar, disminuir, alcanzar o eliminar) seguido por un indicador concreto (por ejemplo: asistencia, aprovechamiento, ... y en algunos casos enfocados a un nivel educativo, a un grado en particular, a una asignatura...), continuando con una meta numérica de mejora del indicador y finalizando con una fecha de cumplimiento máximo (si es para el final del período escolar se puede omitir este elemento dándolo por entendido) </p>
-		</div> -->
-		<p>
-		<img src="<?= base_url('assets/img/mapa/mision.png')?>" width="200" height="200" alt="">
-		<img src="<?= base_url('assets/img/mapa/objetivos.png')?>" width="200" height="200" alt="">
-		<img src="<?= base_url('assets/img/mapa/actividades.png')?>" width="200" height="200" alt="">
-		<img src="<?= base_url('assets/img/mapa/avances.png')?>" width="200" height="200" alt="">
-		<img src="<?= base_url('assets/img/mapa/fin.png')?>" width="100" height="200" alt="">
-		<br>
-		<u><a style="padding-left: 7em; cursor: hand; cursor: pointer; color: blue;" id="img_mision">Ir a misión</a></u>
-		<a style="padding-left: 7em; cursor: hand; cursor: pointer;" >Ir a objetivos</a>
-		<a style="padding-left: 7em; cursor: hand; cursor: pointer;" >Ir a acciones</a>
-		<u><a style="padding-left: 7em; cursor: hand; cursor: pointer; color: blue;" id="img_avances">Ir a avances</a></p></u>
-	<br>
-	
-	</div>
-</div>
 
-<script src="https://unpkg.com/gijgo@1.9.11/js/gijgo.min.js" type="text/javascript"></script>
-<link href="https://unpkg.com/gijgo@1.9.11/css/gijgo.min.css" rel="stylesheet" type="text/css"/>
+	<div class="modal fade bd-example-modal-lg" id="T1_SARAPE" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+		<div class="modal-dialog modal-lg" role="document" style="height: 1200px !important;">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title" id="exampleModalLabel">Tutorial 1</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<div class="row">
+						<div class="col-12" id="VT1">
+							<iframe width="100%" height="415" src="https://www.youtube.com/embed/S5HT3mqxs3w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="modal fade bd-example-modal-lg" id="T2_SARAPE" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+		<div class="modal-dialog modal-lg" role="document" style="height: 1200px !important;">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title" id="exampleModalLabel">Tutorial 2</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<div class="row">
+						<div class="col-12" id="VT2">
+							<iframe width="100%" height="415"  src="https://www.youtube.com/embed/d3yfq2hiuiI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="modal fade bd-example-modal-lg" id="T3_SARAPE" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+		<div class="modal-dialog modal-lg" role="document" style="height: 1200px !important;">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title" id="exampleModalLabel">Tutorial 3</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<div class="row">
+						<div class="col-12" id="VT3">
+							<iframe width="100%" height="415"  src="https://www.youtube.com/embed/xYSsgzHoCpA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="modal fade bd-example-modal-lg" id="T4_SARAPE" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+		<div class="modal-dialog modal-lg" role="document" style="height: 1200px !important;">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title" id="exampleModalLabel">Tutorial 4</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<div class="row">
+						<div class="col-12" id="VT4">
+							<iframe width="100%" height="415"  src="https://www.youtube.com/embed/ysTVJgO31MM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script>
+		$('.close').click(function(event) {
+			$('#VT1').empty();
+			$('#VT1').html('<iframe width="100%" height="415" src="https://www.youtube.com/embed/S5HT3mqxs3w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+			$('#VT2').empty();
+			$('#VT2').html('<iframe width="100%" height="415"  src="https://www.youtube.com/embed/d3yfq2hiuiI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+			$('#VT3').empty();
+			$('#VT3').html('<iframe width="100%" height="415"  src="https://www.youtube.com/embed/xYSsgzHoCpA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+			$('#VT4').empty();
+			$('#VT4').html('<iframe width="100%" height="415"  src="https://www.youtube.com/embed/ysTVJgO31MM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+		});
+	</script>
+	<script src="https://unpkg.com/gijgo@1.9.11/js/gijgo.min.js" type="text/javascript"></script>
+	<link href="https://unpkg.com/gijgo@1.9.11/css/gijgo.min.css" rel="stylesheet" type="text/css"/>
