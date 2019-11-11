9f4d40f3bbfa152c1f82541db2cf68e23fce045e
uziel1111 <uziel.123@gmail.com>
2019-11-11 15:38:09 -0600

    revista nov

--- application/views/index/revistaEscolar.php
+++ application/views/index/revistaEscolar.php
@@ -11,6 +11,12 @@
       <div class="modal-body">
         <div class="container">
           <div class="row">
+            <div class="col-sm-3">
+             <!-- onclick="Index.getinformese(6)" -->
+             <a href="https://issuu.com/ginaorozcoo/docs/revista_noviembre_2019"  target="_blank" title="Edición #15 Noviembre">
+               <img src="<?= base_url('assets/img/revista-noviembre.png'); ?>" class="img-fluid" alt="Responsive image">
+             </a>
+           </div>
              <div class="col-sm-3">
               <!-- onclick="Index.getinformese(6)" -->
               <a href="https://issuu.com/ginaorozcoo/docs/informese_coahuila_octubre_2019"  target="_blank" title="Edición #14 Octubre">
