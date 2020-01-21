67408e97874d4da3381b16dc72737602fc21c5ac
ravd45 <r.velazqez@hotmail.com>
2020-01-21 16:51:25 -0600

    acomodando el frame

--- application/views/index/aprovechamiento_escolar.php
+++ application/views/index/aprovechamiento_escolar.php
@@ -2,11 +2,13 @@
 <br><br><br>
 <div class="container">   
 <div class="card card-style-1">
-        <div class="row">
-            <div class="col-ms-12 offset-2">
+        <div class="row-mb-2">
+            <div class="col-mb-12">
                 <br>
                 <h3>Aprovechamiento escolar</h3>
-               <iframe width="800" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiNzc4MmUwOGEtYzUwMy00MWRjLTkwN2YtMzhiZDEyNGZjMDdhIiwidCI6IjUxZmZlNDRiLTNhMjktNDQ0Ny04YzNiLTE4MzdiODllMDYxNCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+               <div class="offset-1" style="overflow-x: auto;">
+                   <iframe width="930" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiNzc4MmUwOGEtYzUwMy00MWRjLTkwN2YtMzhiZDEyNGZjMDdhIiwidCI6IjUxZmZlNDRiLTNhMjktNDQ0Ny04YzNiLTE4MzdiODllMDYxNCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+               </div>
             </div>
         </div>
     </div>
