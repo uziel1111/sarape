488c587410b9494d91e7bc74d80f7c8753524478
ravd45 <r.velazqez@hotmail.com>
2020-01-27 12:09:15 -0600

    frames primaria y secundaria cambio de urls

--- application/views/aprovechamiento_escolar/primaria_frame.php
+++ application/views/aprovechamiento_escolar/primaria_frame.php
@@ -2,6 +2,6 @@
                 <br>
                 <h3 style="text-align: center">Aprovechamiento escolar - Primaria</h3>
                <div class="offset-1" style="overflow-x: auto;">
-                 <iframe width="930" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiNzc4MmUwOGEtYzUwMy00MWRjLTkwN2YtMzhiZDEyNGZjMDdhIiwidCI6IjUxZmZlNDRiLTNhMjktNDQ0Ny04YzNiLTE4MzdiODllMDYxNCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+                <iframe width="930" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiNzc4MmUwOGEtYzUwMy00MWRjLTkwN2YtMzhiZDEyNGZjMDdhIiwidCI6IjUxZmZlNDRiLTNhMjktNDQ0Ny04YzNiLTE4MzdiODllMDYxNCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
                </div>
             </div>
