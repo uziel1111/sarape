4d685c98d68e55c7e5cc6b59c6de5581ea3628b1
ravd45 <r.velazqez@hotmail.com>
2020-02-17 12:34:35 -0600

    migracion doc  cuda a sarape

--- application/views/cuda/modal_documentos.php
+++ application/views/cuda/modal_documentos.php
@@ -8,15 +8,15 @@
 	
 	<div class="row">
 		<div class="col-sm text-right">
-			<a class="btn btn-success mb-2" target="blank_" href="http://qual-edu.org/levantamiento_de_requerimientos/<?= $value['url_comple']?>" role="button"><i class="fas fa-file-download"></i> Descargar</a>   
+			<a class="btn btn-success mb-2" target="blank_" href="<?=base_url($value['url_comple'])?>" role="button"><i class="fas fa-file-download"></i> Descargar</a>   
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-sm">
 			<?php if ($tipo == 'pdf') { ?>
-				<iframe src="https://qual-edu.org/levantamiento_de_requerimientos/<?= $value['url_comple']?>" width="100%" height="500" style="border: none;"></iframe>
+				<iframe src="<?=base_url($value['url_comple'])?>" width="100%" height="500" style="border: none;"></iframe>
 			<?php } else { ?>
-				<img src="https://qual-edu.org/levantamiento_de_requerimientos/<?= $value['url_comple']?>" width="100%" height="100%" style="border: none;" />
+				<img src="<?= base_url($value['url_comple'])?>" width="100%" height="100%" style="border: none;" />
 			<?php } ?>
 		</div>
 	</div>	
