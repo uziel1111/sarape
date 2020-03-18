18f9ac73925a8490809c3c344b8d7fbf3fa04ace
uziel1111 <uziel.123@gmail.com>
2020-03-18 11:17:11 -0600

    covid

--- assets/js/Utiles.js
+++ assets/js/Utiles.js
@@ -3,7 +3,7 @@ $(function () {
 });
 
 var Utiles = {
-  path_docs : "http://www.sarape.gob.mx/assets/docs/",
+  path_docs : "https://www.sarape.gob.mx/assets/docs/",
 
   showPDF : function(idModal, urlPdf){
     var dom = '<iframe src="'+this.path_docs+urlPdf+'" width="100%" height="500" style="border: none;"></iframe>';
