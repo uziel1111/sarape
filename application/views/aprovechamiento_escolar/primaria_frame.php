2120094391470a34ca045ef297b4b32646906d66
uziel1111 <uziel.123@gmail.com>
2020-05-21 12:56:12 -0500

    ajuste aprovecha

--- application/views/aprovechamiento_escolar/primaria_frame.php
+++ application/views/aprovechamiento_escolar/primaria_frame.php
@@ -1,8 +1,8 @@
  <div class="col-mb-12">
                 <br>
-                <!-- <h3 style="text-align: center">Aprovechamiento escolar - Primaria</h3> -->
-                <h3 style="text-align: center">Informe de aprovechamiento escolar en proceso de actualización para el segundo periodo de evaluaciones, a la brevedad posible el informe estará disponible.</h3>
+                <h3 style="text-align: center">Aprovechamiento escolar - Primaria</h3>
+                <!-- <h3 style="text-align: center">Informe de aprovechamiento escolar en proceso de actualización para el segundo periodo de evaluaciones, a la brevedad posible el informe estará disponible.</h3> -->
                <div class="offset-1" style="overflow-x: auto;">
-                <iframe width="930" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiNzc4MmUwOGEtYzUwMy00MWRjLTkwN2YtMzhiZDEyNGZjMDdhIiwidCI6IjUxZmZlNDRiLTNhMjktNDQ0Ny04YzNiLTE4MzdiODllMDYxNCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+                <iframe width="930" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiYzdlZjUwMjUtZDI5ZS00NWM4LTkwMmMtYjY5MGU1NGViYmE0IiwidCI6IjUxZmZlNDRiLTNhMjktNDQ0Ny04YzNiLTE4MzdiODllMDYxNCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
                </div>
             </div>
--- application/views/aprovechamiento_escolar/secundaria_frame.php
+++ application/views/aprovechamiento_escolar/secundaria_frame.php
@@ -1,8 +1,8 @@
 <div class="col-mb-12">
                 <br>
-                <!-- <h3 style="text-align: center">Aprovechamiento escolar - Secundaria</h3> -->
-                <h3 style="text-align: center">Informe de aprovechamiento escolar en proceso de actualización para el segundo periodo de evaluaciones, a la brevedad posible el informe estará disponible.</h3>
+                <h3 style="text-align: center">Aprovechamiento escolar - Secundaria</h3>
+                <!-- <h3 style="text-align: center">Informe de aprovechamiento escolar en proceso de actualización para el segundo periodo de evaluaciones, a la brevedad posible el informe estará disponible.</h3> -->
                <div class="offset-1" style="overflow-x: auto;">
-                   <iframe width="930" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiNjIzMTRiMTktNjM4Mi00ZDUwLWI2NzYtNzVmMTY2NWViYjczIiwidCI6IjUxZmZlNDRiLTNhMjktNDQ0Ny04YzNiLTE4MzdiODllMDYxNCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+                   <iframe width="930" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiOGU4Yjg1NWEtMzViMi00YTZiLWI5ODktYmFiZTgwNDU5MjU4IiwidCI6IjUxZmZlNDRiLTNhMjktNDQ0Ny04YzNiLTE4MzdiODllMDYxNCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
                </div>
             </div>
