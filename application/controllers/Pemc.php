762526f1d16f18ec0181b732e80836301ea6397f
Jose Luis Sanchez <jlleluis@gmail.com>
2020-07-07 15:30:30 -0500

    funcionalidad

--- application/controllers/Pemc.php
+++ application/controllers/Pemc.php
@@ -128,64 +128,64 @@ class Pemc extends CI_Controller {
 
 //FUNCIONAMIENTO Y VALIDACION PARA SUPERVISOR BY LUIS SANCHEZ... all reserved rights
 //
-				public function verifica_supervisor($cct){
-					$issuper = $this->Pemc_model->valida_supervisor($cct);
-					if(count($issuper) > 0){
-						return TRUE;
-					}else{
-						return FALSE;
-					}
+	public function verifica_supervisor($cct){
+		$issuper = $this->Pemc_model->valida_supervisor($cct);
+		if(count($issuper) > 0){
+			return TRUE;
+		}else{
+			return FALSE;
+		}
+	}
+
+	public function vistas_pemc(){
+		$this->cct = Utilerias::get_cct_sesion($this);
+		$usuario = $this->cct['cve_centro'];
+		$responsables = $this->getPersonal($usuario);
+		$nomenclatura = substr($usuario,0,5);
+		if ($responsables->status==0) {
+			$personas = array();
+		}
+		else {
+			$personas = $responsables->Personal;
+		}
+
+		$options = "";
+		if($responsables->procede == 1 && $responsables->status == 1){
+			foreach ($personas as $persona) {
+				if ($nomenclatura != '05PJN' && $nomenclatura != '05PPR' && $nomenclatura != '05PPS') {
+					$options .= "<option value='{$persona->rfc}'>".$persona->nombre_completo."</option>";
 				}
-
-				public function vistas_pemc(){
-					$this->cct = Utilerias::get_cct_sesion($this);
-					$usuario = $this->cct['cve_centro'];
-					$responsables = $this->getPersonal($usuario);
-					$nomenclatura = substr($usuario,0,5);
-					if ($responsables->status==0) {
-						$personas = array();
-					}
-					else {
-						$personas = $responsables->Personal;
-					}
-
-					$options = "";
-					if($responsables->procede == 1 && $responsables->status == 1){
-						foreach ($personas as $persona) {
-							if ($nomenclatura != '05PJN' && $nomenclatura != '05PPR' && $nomenclatura != '05PPS') {
-								$options .= "<option value='{$persona->rfc}'>".$persona->nombre_completo."</option>";
-							}
-						}
-						$options .="<option value='0'>OTRO</option>";
-					}else{
-						$options .="<option value='0'>OTRO</option>";
-					}
-					$data['responsables'] = $options;
-					$data['idpemc'] = $this->cct['idpemc'];
-					$data['nivel'] = $this->cct['nivel'];//$nivel;
-					$data['nombreuser'] = $this->cct['nombre_centro'];
-					$data['turno'] = $this->cct['turno_single'];
-					$data['cct'] = $this->cct['cve_centro'];
-					$data['director'] = $this->cct['nombre_director'];
-			Utilerias::pagina_basica_pemcv2($this, "pemc/index", $data);
+			}
+			$options .="<option value='0'>OTRO</option>";
+		}else{
+			$options .="<option value='0'>OTRO</option>";
 		}
+		$data['responsables'] = $options;
+		$data['idpemc'] = $this->cct['idpemc'];
+		$data['nivel'] = $this->cct['nivel'];//$nivel;
+		$data['nombreuser'] = $this->cct['nombre_centro'];
+		$data['turno'] = $this->cct['turno_single'];
+		$data['cct'] = $this->cct['cve_centro'];
+		$data['director'] = $this->cct['nombre_director'];
+		Utilerias::pagina_basica_pemcv2($this, "pemc/index", $data);
+	}
 
-		public function obtiene_vista_diagnostico(){
-			$datos_sesion = Utilerias::get_cct_sesion($this);
-			$diagnostico = $this->Pemc_model->obtener_diagnostico_xidpemc($datos_sesion['idpemc']);
-			$data = array('diagnostico' => $diagnostico);
-			$str_vista = $this->load->view("pemc/diagnostico", $data, TRUE);
-			$response = array('str_vista' => $str_vista);
-			Utilerias::enviaDataJson(200, $response, $this);
-			exit;
-		}
+	public function obtiene_vista_diagnostico(){
+		$datos_sesion = Utilerias::get_cct_sesion($this);
+		$diagnostico = $this->Pemc_model->obtener_diagnostico_xidpemc($datos_sesion['idpemc']);
+		$data = array('diagnostico' => $diagnostico);
+		$str_vista = $this->load->view("pemc/diagnostico", $data, TRUE);
+		$response = array('str_vista' => $str_vista);
+		Utilerias::enviaDataJson(200, $response, $this);
+		exit;
+	}
 
-		public function guarda_diagnostico(){
-			$datos_sesion = Utilerias::get_cct_sesion($this);
-			$diagnostico = $this->input->post('in_diag');
-			$estatus = $this->Pemc_model->guarda_diagnostico(strip_tags($diagnostico),$datos_sesion['idpemc']);
-			$response = array('estatus' => $estatus);
-			Utilerias::enviaDataJson(200, $response, $this);
-			exit;
-		}
+	public function guarda_diagnostico(){
+		$datos_sesion = Utilerias::get_cct_sesion($this);
+		$diagnostico = $this->input->post('in_diag');
+		$estatus = $this->Pemc_model->guarda_diagnostico(strip_tags($diagnostico),$datos_sesion['idpemc']);
+		$response = array('estatus' => $estatus);
+		Utilerias::enviaDataJson(200, $response, $this);
+		exit;
+	}
 }
--- assets/js/pemc/principal.js
+++ assets/js/pemc/principal.js
@@ -8,9 +8,34 @@ $("#nv_diagnostico").click(function (e) {
  Principal_pemc.obtiene_vista_diagnostico($("#idpemc").val());
 });
 
+$("#nv_objetivos_metas_acciones").click(function (e) {
+	e.preventDefault();
+ Principal_pemc.obtiene_vista_diagnostico($("#idpemc").val());
+});
+
 var Principal_pemc = {
   	obtiene_vista_diagnostico: (idpemc) => {
     	ruta = base_url + "Pemc/obtiene_vista_diagnostico";
+			$.ajax({
+				url:ruta,
+				data: {idpemc:idpemc},
+				beforeSend: function(xhr) {
+					Notification.loading("");
+				}
+			})
+			.done(function(data){
+				$("#vista_diagnostico").empty();
+				$("#vista_diagnostico").append(data.str_vista);
+			})
+			.fail(function(e) {
+				console.error("Error in ()"); console.table(e);
+			})
+			.always(function() {
+				swal.close();
+			});
+  	},//niveles
+  	obtiene_vista_objetivosymetas: (idpemc) => {
+    	ruta = base_url + "Pemc/obtiene_vista_objetivosymetas";
 			$.ajax({
 				url:ruta,
 				data: {idpemc:idpemc},
