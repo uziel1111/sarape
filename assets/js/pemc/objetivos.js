583018a288814c7972b0fc9cc1edfe8ef941c1b9
Jose Luis Sanchez <jlleluis@gmail.com>
2020-07-08 12:51:22 -0500

    modulo de objetivos

---
+++ assets/js/pemc/objetivos.js
@@ -0,0 +1,29 @@
+
+$("#btn_crear_obj").click(function(){
+	Objetivos.get_view_creareditar_obj();
+});
+
+
+
+var Objetivos = {
+	get_view_creareditar_obj: () => {
+    $.ajax({
+		url:base_url+"Objetivos/get_view_obj",
+		data:{},
+		beforeSend: function(xhr) {
+			Notification.loading("Cargando vista");
+		}
+	})
+	.done(function(data){
+		$("#contenedor_obj_gen").empty();
+		$("#contenedor_obj_gen").append(data.str_view);
+		$("#modal_generico_obj").modal('show');
+	})
+	.fail(function(e) {
+		console.error("Error in ()"); console.table(e);
+	})
+	.always(function() {
+		swal.close();
+	});
+  },
+}
