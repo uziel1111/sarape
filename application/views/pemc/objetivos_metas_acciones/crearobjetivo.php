ef662bada4d6f363e4b3aef8a6fe1768256b1a3f
Jose Luis Sanchez <jlleluis@gmail.com>
2020-07-08 14:08:29 -0500

    ajsutes

--- application/views/pemc/objetivos_metas_acciones/crearobjetivo.php
+++ application/views/pemc/objetivos_metas_acciones/crearobjetivo.php
@@ -2,15 +2,15 @@
 	<form class="form-group" name="form_crear_editar_obj" id="form_crear_editar_obj">
 		<div class="form-group">
 		    <label for="text_objetivo_c">* Objetivo</label>
-		    <textarea class="form-control" id="text_objetivo_c" rows="3"></textarea>
+		    <textarea class="form-control" id="text_objetivo_c" name="text_objetivo_c" rows="3"></textarea>
 		 </div>
 		 <div class="form-group">
 		    <label for="text_meta_c">* Meta</label>
-		    <textarea class="form-control" id="text_meta_c" rows="3"></textarea>
+		    <textarea class="form-control" id="text_meta_c" name="text_meta_c" rows="3"></textarea>
 		 </div>
 		 <div class="form-group">
 		    <label for="text_comentariosG_c">Comentarios generales(opcional)</label>
-		    <textarea class="form-control" id="text_comentariosG_c" rows="3"></textarea>
+		    <textarea class="form-control" id="text_comentariosG_c" name="text_comentariosG_c" rows="3"></textarea>
 		 </div>
 		 <div class="row">
 		 	<div class="col-xs-12 col-sm-12 col-md-3">
