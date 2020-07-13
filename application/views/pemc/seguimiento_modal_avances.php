08d69b83b4ea7154ba5116d94cd770bdb3539f64
uziel1111 <uziel.123@gmail.com>
2020-07-13 09:34:08 -0500

    ajuste card modal seguimiento historial avances

--- application/views/pemc/seguimiento_modal_avances.php
+++ application/views/pemc/seguimiento_modal_avances.php
@@ -1,42 +1,46 @@
 <?php foreach ($arr_datos_accion as $key => $value): ?>
-  <div class="row" style="background-color: #FFCC80; ">
-    <div class="card-header">
-      <b>Objetivo:</b> <?=$value['objetivo'];?>
+  <div class="card">
+  <div class="card-header">
+    <div class="row" style="background-color: #FFCC80; ">
+      <div class="card-header">
+        <b>Objetivo:</b> <?=$value['objetivo'];?>
+      </div>
     </div>
   </div>
-  <div class="row" style="background-color: #bee5eb; ">
-    <div class="col-12">
-      <b>Meta:</b> <?=$value['meta'];?>
+  <div class="card-body">
+    <div class="row" style="background-color: #bee5eb; ">
+      <div class="col-12">
+        <b>Meta:</b> <?=$value['meta'];?>
+      </div>
+    </div>
+    <div class="row" style="background-color: #bee5eb; ">
+      <div class="col-12">
+        <b>Comentarios generales:</b> <?=$value['comentario_general'];?>
+      </div>
+    </div>
+    <div class="row" style="background-color: #c3e6cb; ">
+      <div class="col-12">
+        <b>Ambitos:</b> <?=$value['ambitos'];?>
+      </div>
+    </div>
+    <div class="row">
+      <table class="table table-borderless">
+          <thead>
+            <tr>
+              <th class="text-center">Fechas</th>
+              <th class="text-center">Porcentaje de avance</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach ($arr_avances as $key => $value): ?>
+            <tr>
+              <td class="text-center"><?= $value['fcreacion'];?></td>
+              <td class="text-center"><?= $value['avance'];?>%</td>
+            </tr>
+            <?php endforeach; ?>
+          </tbody>
+        </table>
     </div>
   </div>
-  <div class="row" style="background-color: #bee5eb; ">
-    <div class="col-12">
-      <b>Comentarios generales:</b> <?=$value['comentario_general'];?>
-    </div>
-  </div>
-  <div class="row" style="background-color: #c3e6cb; ">
-    <div class="col-12">
-      <b>Ambitos:</b> <?=$value['ambitos'];?>
-    </div>
-  </div>
-<?php endforeach; ?>
-
-
-  <div class="row">
-    <table class="table table-borderless">
-        <thead>
-          <tr>
-            <th class="text-center">Fechas</th>
-            <th class="text-center">Porcentaje de avance</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php foreach ($arr_avances as $key => $value): ?>
-          <tr>
-            <td class="text-center"><?= $value['fcreacion'];?></td>
-            <td class="text-center"><?= $value['avance'];?>%</td>
-          </tr>
-          <?php endforeach; ?>
-        </tbody>
-      </table>
-  </div>
+</div>
+  <?php endforeach; ?>
