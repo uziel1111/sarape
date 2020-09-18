4ffeffbc48e33ae71c420e5ea03e3621473606ba
uziel1111 <uziel.123@gmail.com>
2020-09-18 08:50:09 -0500

    ajustes CUDA, NO MOSTRABA PDF

--- application/controllers/Cuda.php
+++ application/controllers/Cuda.php
@@ -1,9 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-	
+
 class Cuda extends CI_Controller
 {
-	
+
 	function __construct()
 	{
 		parent::__construct();
@@ -13,18 +13,19 @@ class Cuda extends CI_Controller
 	}
 
 	function index()
-	{	
+	{
 		$data = array();
 		$array_detalles = array();
 		if (isset($_GET['idcuda'])) {
 		$idaplicar = $this->input->get('idcuda');
 		$array_detalles = $this->Cuda_model->getDetalles($idaplicar);
+		echo "<pre>";print_r($array_detalles);die();
 		$data['array_detalles'] = $array_detalles;
 		Utilerias::pagina_basica($this,"cuda/detalles_get", $data);
 		}else{
 			Utilerias::pagina_basica($this, "cuda/cuda", $data);
 		}
-		
+
 	}
 
 	public function getEncuestas()
@@ -36,14 +37,14 @@ class Cuda extends CI_Controller
 		$response = array('str_view' => $str_view);
 		Utilerias::enviaDataJson(200,$response, $this);
 		exit;
-		
+
 	}
 	public function getObjetivo(){
 		$idsubsecretaria = $this->input->post('idsubsecretaria');
 		$array_datos = $this->Cuda_model->getObjetivos($idsubsecretaria);
-		
+
 		$data['array_datos'] = $array_datos;
-		
+
 		$str_view = $this->load->view("cuda/tabla_subsecretaria", $data, TRUE);
 		$response = array('str_view' => $str_view);
 		Utilerias::enviaDataJson(200, $response, $this);
@@ -52,7 +53,7 @@ class Cuda extends CI_Controller
 
 	public function getDetalles(){
 		$idaplicar = $this->input->post('idaplicar');
-		
+
 		$array_detalles = $this->Cuda_model->getDetalles($idaplicar);
 		$data['array_detalles'] = $array_detalles;
 		$str_view = $this->load->view("cuda/modal_detalles", $data, TRUE);
@@ -91,7 +92,7 @@ class Cuda extends CI_Controller
 	{
 		$idusuario = $this->input->post('idusuario');
 		$idsubsecretaria = $this->input->post('idsubsecretaria');
-		
+
 		$array_estadistica = $this->Cuda_model->getEstadisticaUsuario($idusuario);
 		$array_total = $this->Cuda_model->getEstadisticaGeneral($idsubsecretaria);
 		$array_global =  $this->Cuda_model->getEstadisticaGlobal();
@@ -107,7 +108,7 @@ class Cuda extends CI_Controller
 		}
 		$grafica1 = ($estadistica * 100) / $total;
 		$grafica2 = ($estadistica * 100) / $global;
-		
+
 		$data['grafica2'] =  round($grafica2,0);
 		$data['grafica1'] =  round($grafica1,0);
 		$data['sub'] = $total;
@@ -142,7 +143,7 @@ class Cuda extends CI_Controller
 		$titulotema9 = 'Control Escolar / '.$tema9;
 		$tema10 = 0;
 		$titulotema10 = 'Cooperativas y Tiendas Escolares / '.$tema10;
-		
+
 		$respuestaArray = array();
 
 		$idEncuesta = $this->Cuda_model->idEncuestaNivel($nivel, $mes);
@@ -197,18 +198,18 @@ class Cuda extends CI_Controller
 				$titulotema10 = 'Cooperativas y Tiendas Escolares / '.$tema10;
 
 				break;
-				
+
 			}
 
 		}
 
-		
+
 		$temas = array('tema1'=>$titulotema1, 'tema2'=>$titulotema2, 'tema3'=>$titulotema3, 'tema4'=>$titulotema4, 'tema5'=>$titulotema5, 'tema6'=>$titulotema6, 'tema7'=>$titulotema7, 'tema8'=>$titulotema8, 'tema9'=>$titulotema9, 'tema10'=>$titulotema10);
 
 		$data['temas'] = $temas;
 		$data['nivel'] = $nivel;
 		$totalTemas = $tema1 + $tema2 +$tema3+$tema4+$tema5+$tema6+$tema7+$tema8+$tema9+$tema10;
-		
+
 		$str_view = $this->load->view('cuda/consultaNivel', $data, TRUE);
 		$total = $totalTemas;
 		$response = array('str_view' => $str_view, 'total'=>$total);
@@ -218,7 +219,7 @@ class Cuda extends CI_Controller
 
 	function getFormatoTema()
 	{
-		
+
 		$tema = $this->input->post('tema');
 		$nivel = $this->input->post('nivel');
 		$encuestas = array();
@@ -253,7 +254,7 @@ class Cuda extends CI_Controller
 			if (!empty($formatosMes)) {
 			array_push($formatosMesArray, $formatosMes);
 			}
-			
+
 		}
 
 
--- application/models/Cuda_model.php
+++ application/models/Cuda_model.php
@@ -5,7 +5,7 @@ class Cuda_model extends CI_Model
 		parent::__construct();
 		$this->load->database();
 		$this->ci_db = $this->load->database('ci_db', TRUE);
-	}	
+	}
 
 	public function getEncuesta($idusuario)
 	{
@@ -27,7 +27,7 @@ class Cuda_model extends CI_Model
 		$str_query = "SELECT u.* from usuario u
 		inner join aplicar a on a.idusuario = u.idusuario
 		where u.idsubsecretaria ={$idsubsecretria}  and a.estatus = 1 and a.idusuario <> 12 group by a.idusuario;";
-		
+
 		// return $this->db->query($str_query)->result_array();
 		return $this->ci_db->query($str_query)->result_array();
 	}
@@ -35,15 +35,16 @@ class Cuda_model extends CI_Model
 	public function getDetalles($idaplicar)
 	{
 
-		// $str_query = "SELECT respuesta, url_comple, pregunta, idaplicar, GROUP_CONCAT(complemento) AS complemento, 
-		// idpregunta, otroResponsable, responsableDocumento, tema, sostenimiento, direccion, idusuario 
+		// $str_query = "SELECT respuesta, url_comple, pregunta, idaplicar, GROUP_CONCAT(complemento) AS complemento,
+		// idpregunta, otroResponsable, responsableDocumento, tema, sostenimiento, direccion, idusuario
 		// FROM cuda WHERE idaplicar = {$idaplicar} GROUP BY idpregunta;";
-		$str_query = "SELECT r.respuesta, r.url_comple, p.pregunta, r.idaplicar, group_concat(r.complemento) as complemento, r.idpregunta, a.otroResponsable, a.responsableDocumento, a.tema, a.sostenimiento, u.direccion, u.idusuario from respuesta r 
-		INNER JOIN pregunta p on p.idpregunta = r.idpregunta
+		$str_query = "SELECT r.respuesta, r.url_comple, p.pregunta, r.idaplicar, group_concat(r.complemento) as complemento, r.idpregunta, a.otroResponsable, a.responsableDocumento, a.tema, a.sostenimiento, u.direccion, u.idusuario from respuesta r
+		left JOIN pregunta p on p.idpregunta = r.idpregunta
 		INNER JOIN aplicar a on a.idaplicar = r.idaplicar
         INNER JOIN usuario u on a.idusuario = u.idusuario
 		where r.idaplicar = {$idaplicar}  and a.estatus = 1
-		group by r.idpregunta;";
+		group by r.idpregunta";
+		// echo "<pre>";print_r($str_query);die();
 		// return $this->db->query($str_query)->result_array();
 		return $this->ci_db->query($str_query)->result_array();
 	}
@@ -51,19 +52,19 @@ class Cuda_model extends CI_Model
 	{
 		// $str_query = "SELECT url_comple from cuda
 		// where idaplicar = {$idaplicar} and url_comple is not null limit 1; ";
-		$str_query = "SELECT r.url_comple from respuesta r 
+		$str_query = "SELECT r.url_comple from respuesta r
 		where r.idaplicar = {$idaplicar} and r.url_comple is not null; ";
-		
+
 		// return $this->db->query($str_query)->result_array();
 		return $this->ci_db->query($str_query)->result_array();
 	}
 
 	public function getContacto($idusuario)
 	{
-		// $str_query = "SELECT idusuario, nombre, paterno, materno, area_departamento, ntelefono, email 
+		// $str_query = "SELECT idusuario, nombre, paterno, materno, area_departamento, ntelefono, email
 		// from cuda where idusuario = {$idusuario} limit 1;";
 		$str_query = "SELECT * from usuario where idusuario = {$idusuario} ";
-		
+
 		// return $this->db->query($str_query)->result_array();
 		return $this->ci_db->query($str_query)->result_array();
 	}
@@ -75,7 +76,7 @@ class Cuda_model extends CI_Model
 		$str_query = "SELECT count(a.idaplicar) as encuestasUsuario from aplicar a
 		inner join usuario u on u.idusuario = a.idusuario
 		where u.idusuario = {$idusuario} and a.estatus = 1;";
-		
+
 		// return $this->db->query($str_query)->result_array();
 		return $this->ci_db->query($str_query)->result_array();
 	}
@@ -86,7 +87,7 @@ class Cuda_model extends CI_Model
 		$str_query = "SELECT count(a.idaplicar) as total from aplicar a
 		inner join usuario u on u.idusuario = a.idusuario
 		where u.idsubsecretaria = {$idsubsecretaria} and a.estatus = 1;";
-		
+
 		// return $this->db->query($str_query)->result_array();
 		return $this->ci_db->query($str_query)->result_array();
 	}
@@ -95,14 +96,14 @@ class Cuda_model extends CI_Model
 	{
 		// $str_query = "SELECT count(idaplicar) as global from cuda where estatus = 1  and idpregunta = 1;";
 		$str_query = "SELECT count(a.idaplicar) as global from aplicar a where a.estatus = 1";
-		
+
 		// return $this->db->query($str_query)->result_array();
 		return $this->ci_db->query($str_query)->result_array();
 	}
 
 	public function idEncuestaNivel($nivel,$mes)
 	{
-		
+
 		$querynivel = "SELECT tema, idaplicar from cuda
 		where estatus = 1 and complemento ='{$nivel}'";
 
@@ -113,22 +114,22 @@ class Cuda_model extends CI_Model
 		inner join ({$querymes}) as mes on nivel.idaplicar = mes.idaplicar";
 
 		if ($nivel != 'No' && $mes == 'No') {
-			
-			return $this->db->query($querynivel)->result_array();	
+
+			return $this->db->query($querynivel)->result_array();
 		}else {
 			if ($nivel == 'No' && $mes != 'No') {
-				
-				return $this->db->query($querymes)->result_array();	
+
+				return $this->db->query($querymes)->result_array();
 			} else {
 
 				if ($nivel != 'No' && $mes != 'No') {
-					
-					return $this->db->query($querynivelmes)->result_array();	
+
+					return $this->db->query($querynivelmes)->result_array();
 				}
 			}
 		}
 
-		
+
 	}
 
 	public function getFormatoTema($tema, $nivel)
@@ -138,7 +139,7 @@ class Cuda_model extends CI_Model
 		$str_query = "SELECT * from aplicar a
 		inner join respuesta r on r.idaplicar = a.idaplicar
 		where a.tema = {$tema} and r.complemento = '{$nivel}'  and a.estatus = 1;";
-		
+
 		// return $this->db->query($str_query)->result_array();
 		return $this->ci_db->query($str_query)->result_array();
 
@@ -146,13 +147,13 @@ class Cuda_model extends CI_Model
 
 	public function getFormatoTemaMes($idaplicar, $mes)
 	{
-		// $str_query= "SELECT idaplicar from cuda 
+		// $str_query= "SELECT idaplicar from cuda
 		// where idaplicar = {$idaplicar} and complemento = '{$mes}' and estatus = 1 ;";
-		$str_query= "SELECT r.idaplicar from respuesta r 
+		$str_query= "SELECT r.idaplicar from respuesta r
 		where r.idaplicar = {$idaplicar} and r.complemento = '{$mes}';";
-		
+
 		// return $this->db->query($str_query)->result_array();
-	
+
 	return $this->ci_db->query($str_query)->result_array();
 	}
 
