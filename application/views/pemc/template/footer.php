f55b21d60c438373129b73057ba7e95c6d249b40
dafarit13@gmail.com <dafarit13@gmail.com>
2021-03-18 11:51:22 -0600

    Sticky footer

--- application/views/pemc/template/footer.php
+++ application/views/pemc/template/footer.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <!-- Start Cta Area -->
-       <!--<section class="cta-area">
+<!--<section class="cta-area">
                 <div class="container">
                         <div class="row justify-content-center">
                                 <div class="col-lg-8 d-flex no-flex-xs justify-content-center align-items-center">
@@ -9,10 +9,10 @@
                         </div>
                 </div>
         </section> -->
-        <!-- End Cta Area -->
-        <footer  class="section-footer-small" id="footer">
-         <div class="container">
-                 <!--<div class="row">
+<!-- End Cta Area -->
+<footer id="footer">
+    <div class="container">
+        <!--<div class="row">
                     <div class="col-lg-3">
                             <div class="single-footer-widget">
                                 <div class="col-lg-8 d-flex no-flex-xs justify-content-center align-items-center">
@@ -20,14 +20,14 @@
                                 </div>
                             </div>
                     </div>-->
-                    <!--<div class="col-sm-6">
+        <!--<div class="col-sm-6">
                         <div class="single-footer-widget">
                             <h6 class="text-white text-uppercase mb-20">Gobierno del Estado de Coahuila de Zaragoza</h6>
                             <p>Palacio de Gobierno, 1er. Piso, Juárez y Zaragoza s/n,<br> Zona Centro 25000 Saltillo, Coah.<br>
 Teléfonos: (844) 411-8500, 411-8588, 411-8634 Fax. 414-7946</p>
                         </div>
                     </div>-->
-                   <!-- <div class="col-sm-3">
+        <!-- <div class="col-sm-3">
                         <div class="single-footer-widget">
                                     <h6 class="text-white text-uppercase mb-20">Sitios de interés</h6>
                                     <div class="d-flex">
@@ -41,21 +41,22 @@ Teléfonos: (844) 411-8500, 411-8588, 411-8634 Fax. 414-7946</p>
                             </div>
                     </div>
                 </div> -->
-                <div class="d-flex justify-content-between align-items-center" >
-                    <p class="footer-text m-0" style="color: white;">Copyright &copy; 2018  |  Algunos derechos reservados.</p>
-                    <div class="footer-social d-flex align-items-center">
-                            <a href="#"><i class="fab fa-facebook-f"></i></a>
-                            <a href="#"><i class="fab fa-twitter"></i></a>
-                    </div>
-                </div>
+        <div class=" d-flex justify-content-between align-items-center">
+            <p class="footer-text m-0" style="color: white;">Copyright &copy; 2018 | Algunos derechos reservados.</p>
+            <div class="footer-social d-flex align-items-center">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
             </div>
-        </footer>
-  <!-- Footer -->
+        </div>
+    </div>
+</footer>
+<!-- Footer -->
 
-    <!-- script -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="<?= base_url('assets/bootstrap-411/js/bootstrap.min.js'); ?>"></script>
-    <script src="<?= base_url('assets/multiselect/js/bootstrap-select.js'); ?>"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.js" type="text/javascript"></script>
-  </body>
+<!-- script -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="<?= base_url('assets/bootstrap-411/js/bootstrap.min.js'); ?>"></script>
+<script src="<?= base_url('assets/multiselect/js/bootstrap-select.js'); ?>"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.js" type="text/javascript"></script>
+</body>
+
 </html>
